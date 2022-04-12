2fa449ce76e8192fe94e4143aab2529e1c8a5b57
zoeysears <zoey.mae.sears@gmail.com>
2022-04-11 23:37:03 -0500

    Goals for the Week & Beginning Validation on Form

--- recipe_form.js
+++ recipe_form.js
@@ -8,3 +8,26 @@
     adding data to recipe object
     
 */
+
+function validateForm() {
+    //Gets all the values from the form
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+}
+
+//Creates event listener based on user input on page
+function createEventListener() {
+    //Action of clicking on the 'Add Recipe' Button in the top right corner
+    var addRecipeButton = document.getElementById("submit");
+    if(addRecipeButton.addEventListener) {
+        addRecipeButton.addEventListener("click", validateForm, false);
+    } else if(addRecipeButton.attachEvent) {
+        addRecipeButton.attachEvent("click", validateForm);
+    }
+}
+
+if(window.addEventListener) {
+    window.addEventListener("load",createEventListener, false);
+} else if(window.attachEvent) {
+    window.attachEvent("load", createEventListener);
+}
